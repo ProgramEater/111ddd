7c02409bb3889337e520060fcdc3233f5ef8ecf0
ProgramEater <stepanov.s2006@yandex.ru>
2023-02-04 20:26:54 +0300

    1,2,3,4 done

--- main.py
+++ main.py
@@ -13,14 +13,15 @@ def repaint_map():
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 map_params = {
-        "ll": f'37,57',
-        "spn": '0.1,0.1',
-        "l": 'map',
-        'size': '450,450'
-    }
+    "ll": f'37,57',
+    "spn": '0.1,0.1',
+    "l": 'map',
+    'size': '450,450'
+}
 ls = ['map', 'sat', 'sat,skl']
 
 map_file = "map.png"
+repaint_map()
 
 # Инициализируем pygame
 pygame.init()
@@ -31,6 +32,7 @@ screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
 
 coords_shift = 0.01
+shift_scale = 0.5
 
 running = True
 while running:
@@ -42,7 +44,7 @@ while running:
             keys = pygame.key.get_pressed()
             if keys[pygame.K_m]:
                 map_params['l'] = ls[ls.index(map_params['l']) - 1]
-            
+
             if keys[pygame.K_LEFT]:
                 x, y = [float(i) for i in map_params['ll'].split(',')]
                 map_params['ll'] = ','.join((str(x - coords_shift), str(y)))
@@ -55,17 +57,13 @@ while running:
             elif keys[pygame.K_DOWN]:
                 x, y = [float(i) for i in map_params['ll'].split(',')]
                 map_params['ll'] = ','.join((str(x), str(y - coords_shift)))
-            
+
             elif keys[pygame.K_PAGEDOWN]:
-                mp = float(map_params["spn"].split(',')[0]) * 0.5
+                mp = float(map_params["spn"].split(',')[0]) * shift_scale
                 map_params['spn'] = str(mp) + ',' + str(mp)
-                repaint_map()
-                screen.blit(pygame.image.load(map_file), (0, 0))
             elif keys[pygame.K_PAGEUP]:
-                mp = float(map_params["spn"].split(',')[0]) * 2
+                mp = float(map_params["spn"].split(',')[0]) * (1 // shift_scale)
                 map_params['spn'] = str(mp) + ',' + str(mp)
-                repaint_map()
-                screen.blit(pygame.image.load(map_file), (0, 0))
             repaint_map()
             screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
