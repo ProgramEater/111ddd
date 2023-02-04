52e7bb8abe0a2e68bb80e3cece0e944387af432f
ProgramEater <stepanov.s2006@yandex.ru>
2023-02-04 19:09:11 +0300

    2

--- main.py
+++ main.py
@@ -4,26 +4,44 @@ import sys
 import pygame
 import requests
 
+
+def repaint_map():
+    response = requests.get(map_api_server, params=map_params)
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 map_params = {
-        "ll": '57,37',
-        "spn": '0.05,0.05',
-        "l": 'map'
+        "ll": f'37,57',
+        "spn": '0.1,0.1',
+        "l": 'map',
+        'size': '450,450'
     }
-response = requests.get(map_api_server, params=map_params)
+ls = ['map', 'sat', 'sat,skl']
+
 map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
 
 # Инициализируем pygame
 pygame.init()
-screen = pygame.display.set_mode((600, 450))
+screen = pygame.display.set_mode((450, 450))
 # Рисуем картинку, загружаемую из только что созданного файла.
 screen.blit(pygame.image.load(map_file), (0, 0))
 # Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_m]:
+                print('ada')
+                map_params['l'] = ls[ls.index(map_params['l']) - 1]
+                repaint_map()
+                screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
