3e3f54b9bc81a4ecea9823702d4e648515f2e3c9
ProgramEater <stepanov.s2006@yandex.ru>
2023-02-04 18:35:17 +0300

    adfaf

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+import pygame
+import requests
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+map_params = {
+        "ll": '57,37',
+        "spn": '0.05,0.05',
+        "l": 'map'
+    }
+response = requests.get(map_api_server, params=map_params)
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
